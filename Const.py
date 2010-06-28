66258bd9579cb8c72228140cebcb87437d9886d2
Géraud Le Falher <Graud Le Falher daureg@gmail.com>
2010-06-28 17:14:48 +0200

    reorganize Const, change font and add INIT_TOWER option

--- Const.py
+++ Const.py
@@ -20,21 +20,38 @@ pygame.mixer.init()
 PLAYER1=available_mode(options.PLAYER1)
 PLAYER2=available_mode(options.PLAYER2)
 TOWER_VICTORY=clamp(options.TOWER_VICTORY, 20, 500)
-INIT_TOWER=TOWER_VICTORY/10
-RESSOURCE_VICTORY=int(clamp(options.RESSOURCE_VICTORY, 35, 750)
-INIT_VAL=int(clamp(options.INIT_VAL, 3, 75))
-INIT_INC=int(clamp(options.INIT_INC, 1, 7))
+INIT_TOWER=clamp(options.INIT_TOWER, 2, 50)
+RESSOURCE_VICTORY=clamp(options.RESSOURCE_VICTORY, 35, 750)
+INIT_VAL=clamp(options.INIT_VAL, 3, 75)
+INIT_INC=clamp(options.INIT_INC, 1, 7)
 RESX=clamp(options.RESX, 100, 3000)
 RESY=clamp(options.RESY, 100, 3000)
 FULLSCREEN=bool(options.FULLSCREEN)
 
-# Private
-CARD_HEIGHT=int(RESY/3)
+# Dimensions
+CARD_HEIGHT=RESY/3
+ROOF_HEIGHT=RESY/8
+# Colors
+DARK=(0,0,0)
+LIGHT_DARK=(128,0,0)
+GREY=(128,128,128)
+ALPHA_GREY=(128,128,128,128)
+RED=(255,0,0)
+LIGHT_RED=(128,0,0)
+GREEN=(0,255,0)
+LIGHT_GREEN=(0,128,0)
+BLUE=(0,0,255)
+LIGHT_BLUE=(0,0,128)
 LEFT_COLOR=(255,0,0)
 RIGHT_COLOR=(0,0,255)
-BUILDING_FONT=pygame.font.SysFont("gentium", 20, True)
-INC_FONT=pygame.font.SysFont("gentium", 20, True)
-VAL_FONT=pygame.font.SysFont("gentium", 14, True)
-ROOF_HEIGHT=int(RESY/8)
+# Fonts
+FONT_FILE="DejaVuLGCSans.ttf"
+BUILDING_FONT=pygame.font.Font(FONT_FILE, 20)
+INC_FONT=pygame.font.Font(FONT_FILE, 20)
+VAL_FONT=pygame.font.Font(FONT_FILE, 14)
+CARD_FONT=pygame.font.Font(FONT_FILE, 10)
+# Sounds
 RES_POS_SOUND=pygame.mixer.Sound("tada.wav")
 RES_NEG_SOUND=pygame.mixer.Sound("tada.wav")
+BUILD_POS_SOUND=pygame.mixer.Sound("tada.wav")
+BUILD_NEG_SOUND=pygame.mixer.Sound("tada.wav")
