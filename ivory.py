de1dc23dee1440a5b91f3b827394c62aff4106b9
Géraud Le Falher <Graud Le Falher daureg@gmail.com>
2010-06-28 16:37:44 +0200

    more simple launcher

--- ivory.py
+++ ivory.py
@@ -1,8 +1,4 @@
 #! /usr/bin/env python
 # vim: set fileencoding=utf-8 :
-# pylint: disable-msg=W0312
-"""Pygame Arcomage."""
 import Game
-if __name__ == "__main__":
-	g = Game.Game()
-	g.run()
+Game.Game().run()
