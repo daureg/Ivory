1669fd2b8dbafbcefa9f7ad80543bf5c0b8de733
Géraud Le Falher <Graud Le Falher daureg@gmail.com>
2010-07-03 13:55:38 +0200

    add a Deck class

---
+++ Deck.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# pylint: disable=W0312,W0612
+"""Implementation of Deck object."""
+import os, os.path, random, Card, Const
+class Deck:
+	"""Construct a list of cards while maintaining information over those which
+are in game."""
+	def __init__(self, exemplary = 3):
+		"""Reads cards in CARDS_DIR and build them exemplary times."""
+		available = (c for c in os.listdir(Const.CARDS_DIR) if c.endswith(".card"))
+		self.cards = []
+		for i in available:
+			for j in xrange(1, exemplary):
+				self.cards.append(Card.Card(os.path.join(Const.CARDS_DIR, i)))
+		self.ingame = []
+		self.rnd = random.Random()
+
+	def get_card(self):
+		"""Give a random card and place it ingame."""
+		idx = self.rnd.randint(0, len(self.cards) - 1)
+		card = self.cards[idx]
+		self.cards.remove(card)
+		self.ingame.append(card)
+		return card
+
+	def played_card(self, card):
+		"""Return the pos of a played card."""
+		self.ingame.remove(card)
+		self.cards.append(card)
+		return card.box_pos
