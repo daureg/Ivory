e6911aeb4906881752e618c817c361dfb3a32c65
Géraud Le Falher <Graud Le Falher daureg@gmail.com>
2010-07-03 13:17:53 +0200

    pylint

--- Ressource.py
+++ Ressource.py
@@ -1,58 +1,59 @@
 #! /usr/bin/env python
 # vim: set fileencoding=utf-8 :
-# pylint: disable-msg=W0312
+# pylint: disable=W0312
+"""Ressource implementation"""
 import Const, pygame
 class Ressource:
+	"""Handle drawing and counting of a specific ressource."""
 	def __init__(self, pos, color, text, image=""):
+		"""Build the 'text' ressource in 'pos' with 'color'."""
 		self.box_pos = pos
-		self.w = int(Const.RESX/8)
-		self.h = int(Const.RESY/7)
-		x=self.box_pos[0]
-		y=self.box_pos[1]
-		w=self.w
-		h=self.h
+		self.width = Const.RESX/8
+		self.height = Const.RESY/7
+		x = self.box_pos[0]
+		y = self.box_pos[1]
+		w = self.width
+		h = self.height
 		self.color = color
 		self.desc = text
 		#self.pic = pygame.image.load(image)
-		#self.pic_pos = (x,y)
+		#self.pic_pos = (x, y)
 		self.pos_sound = Const.RES_POS_SOUND
 		self.neg_sound = Const.RES_NEG_SOUND
 		self.inc = Const.INIT_INC
 		self.val = Const.INIT_VAL
 		self.inc_font = Const.INC_FONT
-		self.inc_text = self.inc_font.render("%d"%self.inc, True, (255,255,255))
-		self.inc_text_pos = (int(x+w/10),int(y+h/10))
+		self.inc_text = self.inc_font.render("%d"%self.inc, True, (255, 255, 255))
+		self.inc_text_pos = (x+w/10, y+h/10)
 		self.val_font = Const.VAL_FONT
-		self.val_text = self.val_font.render("%d %s"%(self.val,self.desc), True, (255,255,255))
-		self.val_text_pos = (int(x+w/15),int(y+7*h/10))
-		self.box = pygame.Surface((w,h))
+		self.val_text = self.val_font.render("%d %s"%(self.val, self.desc),
+				True, (255, 255, 255))
+		self.val_text_pos = (x+w/15, y+8*h/10)
+		self.box = pygame.Surface((w, h))
 		self.box.fill(self.color)
 	
 	def draw(self, screen):
-		screen.blit(self.box,self.box_pos)
-		screen.blit(self.inc_text,self.inc_text_pos)
-		screen.blit(self.val_text,self.val_text_pos)
-		#screen.blit(self.pic,self.pic_pos)
+		"""Draw the ressource."""
+		screen.blit(self.box, self.box_pos)
+		screen.blit(self.inc_text, self.inc_text_pos)
+		screen.blit(self.val_text, self.val_text_pos)
+		#screen.blit(self.pic, self.pic_pos)
 
 	def change_inc(self, value):
+		"""Modify the incrementation rate of the ressource."""
 		self.inc = max(1, self.inc + value)
-		self.inc_text = self.inc_font.render("%d"%self.inc, True, (255,255,255))
-		if value>0:
+		self.inc_text = self.inc_font.render("%d"%self.inc, True, (255, 255, 255))
+		if value > 0:
 			self.pos_sound.play()
-		if value<0:
+		if value < 0:
 			self.neg_sound.play()
 	
 	def change_val(self, value):
-		self.val = min(max(0, self.val + value), Const.RESSOURCE_VICTORY)
-		self.val_text = self.val_font.render("%d %s"%(self.val,self.desc), True, (255,255,255))
-		if value>0:
+		"""Modify the value of the ressource."""
+		self.val = Const.clamp(self.val + value, 0, Const.RESSOURCE_VICTORY)
+		self.val_text = self.val_font.render("%d %s"%(self.val, self.desc),
+				True, (255, 255, 255))
+		if value > 0:
 			self.pos_sound.play()
-		if value<0:
+		if value < 0:
 			self.neg_sound.play()
-	
-	def val(self):
-		return self.val
-	
-	def inc(self):
-		return self.inc
-	
